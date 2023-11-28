8d757f7b911475edad7e28cadda65d4c29acf9d1
Hakoyu <Hakoyu@outlook.com>
2023-11-29 00:05:04 +0800

    新增 IsBoolConverter

---
+++ Converters/IsBool.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValueConverters;
+
+namespace HKW.WPF.Converters;
+
+/// <summary>
+/// 是布尔转换器
+/// </summary>
+public class IsBoolConverter : NullToBoolConverter
+{
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
+    protected override object Convert(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture
+    )
+    {
+        if (value.Equals(true ^ IsInverted))
+            return true;
+        return false;
+    }
+}
