90981eb9d5d75ec23e3ef3e9f22beb6a2c3deee9
Hakoyu <Hakoyu@outlook.com>
2024-12-08 07:10:09 +0800

    `EnumsToEnumInfosConverter`新增`OnlyValidProperty`,可设置只显示有效枚举值

--- HKW.WPF/Converters/ValueConverters/EnumsToEnumInfosConverter.cs
+++ HKW.WPF/Converters/ValueConverters/EnumsToEnumInfosConverter.cs
@@ -1,4 +1,7 @@
-﻿namespace HKW.WPF.Converters;
+﻿using System.ComponentModel;
+using HKW.CommonValueConverters;
+
+namespace HKW.WPF.Converters;
 
 /// <summary>
 /// 枚举到枚举信息转换器
@@ -8,6 +11,28 @@ public class EnumsToEnumInfosConverter : ValueConverterBase
     /// <inheritdoc/>
     public EnumsToEnumInfosConverter()
     {
-        CommonValueConverter = new CommonValueConverters.EnumsToEnumInfosConverter();
+        CommonValueConverter = new CommonValueConverters.EnumsToEnumInfosConverter()
+        {
+            GetOnlyValid = () => OnlyValid
+        };
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static readonly CommonDependencyProperty<bool> OnlyValidProperty =
+        CommonDependencyProperty.Register<EnumsToEnumInfosConverter, bool>(
+            nameof(OnlyValid),
+            false
+        );
+
+    /// <summary>
+    /// 只显示有效值
+    /// </summary>
+    [DefaultValue(true)]
+    public bool OnlyValid
+    {
+        get => GetValue(OnlyValidProperty);
+        set => SetValue(OnlyValidProperty, value);
     }
 }
